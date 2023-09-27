14505693ea3e0b16f9be7e7105f0ee09bfb14d62
mustafa_r <mustafa@directroutes.net>
2023-09-27 16:17:38 +0600

    added more project

---
+++ GNEW_CDR_decoder_Ringtech/GNEW_CDR_decoder_Ringtech/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GNEW_CDR_decoder_Ringtech
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string fileLocation = "000197-20230904124050.cdr";
+            string[] linesAsString = File.ReadAllLines(fileLocation);
+            string[] line;
+
+            foreach (string ln in linesAsString)
+            {
+                //lines.Add(line.Split(','));
+                line = ln.Split(',');
+
+                string startTime = line[11].Trim();
+                if (!string.IsNullOrEmpty(startTime))
+                {
+                    startTime = parseStringToDate(startTime).ToString("yyyy-MM-dd HH:mm:ss");
+                }
+
+                string endTime = line[11].Trim();
+                if (!string.IsNullOrEmpty(endTime))
+                {
+                    endTime = parseStringToDate(endTime).ToString("yyyy-MM-dd HH:mm:ss");
+                }
+
+                string originatingCallingNumber = line[9];
+                string originatingCalledNumber = line[10];
+
+                string terminatingCallingNUmber = line[9];
+                string terminatingCalledNUmber = line[10];
+
+                string startTime1 = startTime;
+                string endTime1 = endTime;
+
+                string callinigIp = line[13];
+                string calledIp = line[14];
+
+                string TrunkGroupName = line[47];
+
+                string duration = line[53].Trim();
+                ;
+            }
+
+
+
+        }
+        private static DateTime parseStringToDate(string timestamp)  //20181028051316400 yyyyMMddhhmmssfff
+        {
+            DateTime dateTime;
+            if (DateTime.TryParseExact("20230904123527", "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime)) 
+                Console.WriteLine("valid date time format");
+            return dateTime;
+        }
+    }
+}
---
+++ WTL_CDR_Decoder_Teleplus/WTL_CDR_Decoder_Teleplus/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WTL_CDR_Decoder_Teleplus
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string fileLocation = "20230904";            
+            string[] linesAsString = File.ReadAllLines(fileLocation);
+            string[] line;
+
+            foreach (string ln in linesAsString)
+            {
+                //lines.Add(line.Split(','));
+                line = ln.Split(',');
+                string unixCallTime = line[1].Trim();      //unix 
+                string callTime = line[4].Trim();
+                string callDate = line[5].Trim();
+                string duration = line[2].Trim();
+                string bLegTelephoneNumber = line[8].Trim();
+                string cost_of_the_call = line[12].Trim();
+                string bLegRoutingID = line[38].Trim();
+                string callID = line[44].Trim();
+                //string exactCallDuration = line[60].Trim();
+                string ipAddrsOfCaller = line[61].Trim();
+                string originalDDInumber = line[62].Trim();
+
+                //string originatingCallingNumber = line[66];  
+                //string originatingCalledNumber = line[68];  
+
+                //string terminatingCallingNUmber = line[82];
+                //string terminatingCalledNUmber = line[84];
+            }
+
+
+            
+        }
+    }
+}
